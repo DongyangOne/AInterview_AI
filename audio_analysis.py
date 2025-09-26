0d4696293c712d9e7efc2486d2a03afd444fbc79
최혜림 <101788177+hyerim03@users.noreply.github.com>
2025-09-26 15:28:00 +0900

    fix: 모델명 수정

--- audio_analysis.py
+++ audio_analysis.py
@@ -22,7 +22,7 @@ generation_config = {
 }
 
 gemini_model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-2.0-flash-001",
     generation_config=generation_config
 )
 
